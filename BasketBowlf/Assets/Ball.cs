d754e27b7ccbdcc2c1a68545cfb6ad7669797f9d
JustBeachy <ajbeach67@gmail.com>
2022-06-12 20:54:53 -0400

    Bowling Prototype

---
+++ BasketBowlf/Assets/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector2 mouseStartPos, mouseEndPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 0, 300), ForceMode.Impulse);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            mouseStartPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseEndPos = Input.mousePosition;
+
+            rb.AddForce(new Vector3((mouseEndPos.x - mouseStartPos.x)*2, 0, (mouseEndPos.y - mouseStartPos.y)*4) *2);
+            //rb.AddTorque(new Vector3(Mathf.Abs(mouseEndPos.y - mouseStartPos.y)*100, 0, (mouseStartPos.x - mouseEndPos.x) * 100) * 2);
+        }
+
+
+    }
+}
